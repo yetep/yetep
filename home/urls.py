637bcfb985cfd99ff41a0c1cbdab07445b38feaf
Yetep <yetep@gmail.com>
2016-06-11 15:19:20 -0700

    Added page views for links

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^blog/', views.blog, name='blog'),
+    url(r'^about/', views.about, name='about'),
+    url(r'^contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html', {})
+
+def blog(request):
+    return render(request, 'home/blog.html', {})
+
+def about(request):
+    return render(request, 'home/about.html', {})
+
+def contact(request):
+    return render(request, 'home/contact.html', {})
