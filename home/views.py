0bfcc2f50b0d9a9878e47b15be32af1a76f2d0b1
Yetep <yetep@gmail.com>
2016-06-12 22:22:53 -0700

    Added base template for the Blog page

--- home/views.py
+++ home/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def home(request):
     return render(request, 'home/home.html', {})
 
-def blog(request):
-    return render(request, 'home/blog.html', {})
+#def blog(request):
+#    return render(request, 'home/blog.html', {})
 
 def about(request):
     return render(request, 'home/about.html', {})
 
 def contact(request):
     return render(request, 'home/contact.html', {})
+
+def blog(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'home/blog.html', {'posts': posts})
